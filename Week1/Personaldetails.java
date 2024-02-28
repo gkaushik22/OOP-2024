23ee69c45b50f19a8bc2eac48e112a90694b6977
gkaushik22 <gkaushik22@tbc.edu.np>
2024-02-28 14:02:20 +0545

    Personaldetails

---
+++ Week1/Personaldetails.java
@@ -0,0 +1,8 @@
+public class Personaldetails{
+    public static void main(String[] args) {
+        System.out.println("Name: Kaushik Gupta");
+        System.out.println("Address: Baneshwor,Kathmandu");
+        System.out.println("Age: 21");
+        System.out.println("Phone number: +977 9819287650");
+    }
+}
