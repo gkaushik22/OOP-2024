6c61da7a6a8c24863398907037b508dca2673124
gkaushik22 <gkaushik22@tbc.edu.np>
2024-02-29 12:50:59 +0545

    Task1

---
+++ Week2/Task1/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\n///////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+        System.out.println("==\t\t\tStudent Points\t\t\t==");
+        System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\///////////////////\n");
+        System.out.println("Name\t\t\tLab\t\tBonus\tTotal");
+        System.out.println("____\t\t\t___\t\t_____\t_____");
+        System.out.println("Pravesh\t\t\t"+"50\t\t"+"50\t\t"+"100");
+        System.out.println("Sabin\t\t\t"+"48\t\t"+"42\t\t"+"90");
+        System.out.println("Pratham\t\t\t"+"40\t\t"+"40\t\t"+"80");
+        System.out.println("Kaushik\t\t\t"+"45\t\t"+"30\t\t"+"75");
+        System.out.println("Pawan\t\t\t"+"45\t\t"+"35\t\t"+"80");
+
+
+
+    }
+}
